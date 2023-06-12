5de87673429fc830422978e285df21059f373ca8
emanueletoffolon <emanuele.toffolon@enesi.it>
2023-06-12 15:54:20 +0200

    Update test.php

--- examples/test.php
+++ examples/test.php
@@ -4,10 +4,10 @@ use Emanueletoffolon\MysqlIseed\ISeed;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$iseed = new ISeed(['host'=>'localhost','db'=>'database_name','username'=>'database_username','password'=>'database_password','path_files'=>__DIR__.'/seeders/']);
+$iseed = new ISeed(['host'=>'localhost','db'=>'gieffeco_db','username'=>'homestead','password'=>'secret','path_files'=>__DIR__.'/seeders/']);
 
-$iseed->addTables('*');
+$iseed->addTables(['linguaggio','linguaggio_valori_text_lang']);
 $iseed->generate();
 
-//$iseed->load();
+$iseed->load();
 
